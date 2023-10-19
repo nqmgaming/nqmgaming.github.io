a652a089279696a0e7a604f3a69c39a6a4860043
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2023-10-19 14:30:05 +0700

    Hi

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const nameGirl = 'Em';
-const giftUrl = 'https://www.facebook.com/cence.hens';
+const nameGirl = 'Thuỳ';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100027091096229';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng emBe';
+const titleCard = 'Tặng Thuỳ Nek';
 const contentCard = 'Chúc em 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu em 3000.';
 
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
