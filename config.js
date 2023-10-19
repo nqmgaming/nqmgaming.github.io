870464df626b7f3331529ab3ed1132bc270569e9
Daocon <134820492+Daocon@users.noreply.github.com>
2023-10-19 13:24:26 +0700

    Add files via upload
    demo

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/cence.hens';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng emBe';
+const contentCard = 'Chúc em 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu em 3000.';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
