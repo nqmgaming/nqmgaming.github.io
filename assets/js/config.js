c8f8eb4f66336b9bc4d2ddb85765909b1afca9b3
Nguyễn Quang Minh (NQM) <94773751+nqmgaming@users.noreply.github.com>
2023-10-20 13:24:21 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Thuỳ';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100027091096229';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng Thuỳ Nek';
-const contentCard = 'Hi Thuỳ! 20-10 tui chúc Thuỳ có một ngày 20-10 thật vui vẻ và ý nghĩa nhé. Chúc Thuỳ luôn xinh đẹp và thành công trong cuộc sống.' +
+const contentCard = 'Hi Thuỳ! Tui chúc Thuỳ có một ngày 20-10 thật vui vẻ và ý nghĩa nhé. Chúc Thuỳ luôn xinh đẹp và thành công trong cuộc sống.' +
     'Nếu Thuỳ có điều gì vui vẻ hay buồn hoặc chuyện gì đó thì đừng quên chia sẻ với tui nhé, tui sẽ luôn lắng nghe và giúp đỡ thuỳ mọi lúc mọi nơi tui có thể. Yêu Thuỳ nhiều nhiều nhiều <3. ';
 
 const giftImage = 'assets/images/hot-girl.jpg';
