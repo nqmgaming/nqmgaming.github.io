36f16c1996fee6755f883633ccec25d8ecfbf28d
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2023-10-20 13:11:06 +0700

    hi

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Thuỳ';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100027091096229';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Thuỳ Nek';
+const contentCard = 'Hi! Chúc Thuỳ có một ngày 20-10 thật vui vẻ và ý nghĩa nhé. Tui hi vọng thuỳ sẽ luôn vui vẻ và hạnh phúc bên gia đình và người thân. Chúc Thuỳ luôn xinh đẹp và thành công trong cuộc sống.' +
+    'Nếu Thuỳ có điều gì vui vẻ hay buồn hoặc chuyện gì đó thì đừng quên chia sẻ với tui nhé, tui sẽ luôn lắng nghe và giúp đỡ thuỳ mọi lúc mọi nơi. Yêu Thuỳ nhiều nhiều nhiều <3. ';
+
+const giftImage = 'assets/images/hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
